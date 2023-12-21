581fd06e80c87c6f520a0cd9a7d8c27fc46efcf5
Andrii Rudyk <issumbosi@gmail.com>
2023-12-21 11:55:18 +0200

    upd2

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
 const cors = require("cors");
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
+  res.status(200).json({ message: "Ok" });
+});
+
+app.post("/", (req, res) => {
   res.status(200).json({ message: "Ok" });
 });
 
