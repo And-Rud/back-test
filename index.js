9ee809388d5a69e6e6674d4dfecef83599438c7c
Andrii Rudyk <issumbosi@gmail.com>
2023-12-21 12:32:48 +0200

    upd2

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Ok" });
+  res.status(200).json({ message: "Ok2" });
 });
 
 app.post("/", (req, res) => {
-  res.status(200).json({ message: "Ok" });
+  res.status(200).json({ message: "Ok3" });
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
